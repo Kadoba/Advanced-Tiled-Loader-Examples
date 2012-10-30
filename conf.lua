a0561c137282704086d4023807adc05e269bdc7e
Casey Baxter <Casey_Baxter@Hotmail.com>
2012-10-30 11:10:19 -0500

    Updated for 0.12.1

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
-	t.screen.vsync = false
-	t.title = "Advanced Tiled Loader Example"
+    t.screen.vsync = false
+    t.title = "Advanced Tiled Loader Example"
     t.author = "Casey Baxter (Kadoba)"
 end
+
